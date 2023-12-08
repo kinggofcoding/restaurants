2c3cbabc4d33ea71a2cccf12323be97bda74fc9f
unknown <intuitionpz@gmail.com>
2023-12-08 15:15:49 +0800

    build create&read restaurant routes

--- app.js
+++ app.js
@@ -2,53 +2,96 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
-const restaurants = require('./public/jsons/restaurant.json').results
+// const restaurants = require('./public/jsons/restaurant.json').results
 const cssIndex = '/stylesheets/index.css' // index所使用的css
 const cssShow = '/stylesheets/show.css' // show所使用的css
+const methodOverride = require('method-override')
+const db = require('./models')
+const Restaurant = db.Restaurant
 
 // express初始化設定: template,static file, view path,post數據解析
 app.use(express.urlencoded({ extended: true}))
+app.use(methodOverride('_method'))
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
 
-// 接收請求並處理: /
-// 將資料放入index.hbs並返回給客戶端
-app.get('/', (req, res) => {
-  res.render('index', { restaurants, cssPath: cssIndex })
+// 讀取所有餐廳
+app.get('/restaurants', async (req, res) => {
+  try {
+    const restaurants = await Restaurant.findAll({ raw: true })
+    res.render('index', { cssPath: cssIndex, restaurants })
+  } catch (error) {
+    res.status(422).json(error)
+  }
 })
+
+
+// 新增餐廳頁面
+app.get('/restaurants/new', (req, res) => {
+  try {
+    res.render('new')
+  } catch (error) {
+    res.status(422).json(error)
+  }
+})
+
+
+// 讀取單一餐廳
+app.get('/restaurants/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const restaurant = await Restaurant.findByPk(id, { raw: true })
+    res.render('show', { cssPath: cssShow, restaurant })  
+  } catch (error) {
+    res.status(422).json(error)
+  }
+})
+
+// 新增餐廳
+app.post('/restaurants', async (req, res) => {
+  try {
+    const { name, name_en, category, image, location, google_map, phone, rating, description } = req.body
+    const restaurant = await Restaurant.create({ name, name_en, category, image, location, google_map, phone, rating: parseFloat(rating), description })
+    if (restaurant) {
+      res.redirect('/restaurants')
+    }
+  } catch (error) {
+    res.status(422).json(error)
+  }
+})
+
+// 編輯餐廳頁面
+
+// 更新餐廳
+
+// 刪除餐廳
+
+
 
 // 接收請求並處理: /restaurants
 // 如果在search bar有輸入關鍵字，將餐廳列表設定為符合條件之餐廳，否則為所有餐廳列表
 // 將資料放入index.hbs並返回給客戶端
-app.get('/restaurants', (req, res) => {
-  const keyword = req.query.keyword?.trim()
-  const matchedRestaurants = keyword
-    ? restaurants.filter((restaurant) =>
-        Object.values(restaurant).some((property) => {
-          if (typeof property === 'string') {
-            return property.toLowerCase().includes(keyword.toLowerCase())
-          }
-          return false
-        })
-      )
-    : restaurants
-  res.render('index', {
-    restaurants: matchedRestaurants,
-    cssPath: cssIndex,
-    keyword,
-  })
-})
+// app.get('/restaurants', (req, res) => {
+//   const keyword = req.query.keyword?.trim()
+//   const matchedRestaurants = keyword
+//     ? restaurants.filter((restaurant) =>
+//         Object.values(restaurant).some((property) => {
+//           if (typeof property === 'string') {
+//             return property.toLowerCase().includes(keyword.toLowerCase())
+//           }
+//           return false
+//         })
+//       )
+//     : restaurants
+//   res.render('index', {
+//     restaurants: matchedRestaurants,
+//     cssPath: cssIndex,
+//     keyword,
+//   })
+// })
 
-// 接收請求並處理: /restaurant/:id
-// 依照請求路徑上的id顯示對應的餐廳資訊
-// 將資料放入show.hbs並返回給客戶端
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  const restaurant = restaurants.find((restaurant) => restaurant.id.toString() === id)
-  res.render('show', { restaurant, cssPath: cssShow })
-})
 
 // 伺服器啟動並監聽port:3000
 app.listen(port, () => {
