0d6dcf4b5f5071235e4525efe1adaae35a61c257
unknown <intuitionpz@gmail.com>
2023-12-08 10:46:35 +0800

    create seed:restaurant

--- seeders/20231208015732-initial-data.js
+++ seeders/20231208015732-initial-data.js
@@ -1,12 +1,20 @@
 'use strict'
-const restaurants = require('./public/jsons/restaurant.json').results
+const restaurants = require('../public/jsons/restaurant.json').results
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Todos',
+    await queryInterface.bulkInsert('Restaurants',
       restaurants.map((restaurant) =>
         ({
-          name: `todo-${i}`,
+          name: restaurant.name,
+          name_en: restaurant.name_en,
+          category: restaurant.category,
+          image: restaurant.image,
+          location: restaurant.location,
+          phone: restaurant.phone,
+          google_map: restaurant.google_map,
+          rating: restaurant.rating,
+          description: restaurant.description,
           createdAt: new Date(),
           updatedAt: new Date()
         })
