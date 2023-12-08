1002a0aab65d1704dbd5a7761e1752d97823977b
unknown <intuitionpz@gmail.com>
2023-12-08 09:54:52 +0800

    sequelize init

---
+++ README.md
@@ -0,0 +1,34 @@
+# Restaurant List
+A simple web application for restaurant fanatic
+
+## Features
+- listing restaurants from restaurant api
+- searching restaurants
+
+### Searching
+type the keyword of the restaurants in search bar
+
+### Installing
+
+1.Clone the repository
+
+```
+git clone https://github.com/kinggofcoding/restaurant-list.git
+```
+2.Move to the restaurantList directory
+
+```
+cd restaurant-list
+```
+3.Install npm packages
+
+```
+npm install
+```
+4.Launch the application
+
+```
+npm start
+```
+
+5.Server is running on http://localhost:3000 .
---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
+const restaurants = require('./public/jsons/restaurant.json').results
+const cssIndex = '/stylesheets/index.css' // index所使用的css
+const cssShow = '/stylesheets/show.css' // show所使用的css
+
+// express初始化設定: template,static file, view path
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+app.use(express.static('public'))
+
+// 接收請求並處理: /
+// 將資料放入index.hbs並返回給客戶端
+app.get('/', (req, res) => {
+  res.render('index', { restaurants, cssPath: cssIndex })
+})
+
+// 接收請求並處理: /restaurants
+// 如果在search bar有輸入關鍵字，將餐廳列表設定為符合條件之餐廳，否則為所有餐廳列表
+// 將資料放入index.hbs並返回給客戶端
+app.get('/restaurants', (req, res) => {
+  const keyword = req.query.keyword?.trim()
+  const matchedRestaurants = keyword
+    ? restaurants.filter((restaurant) =>
+        Object.values(restaurant).some((property) => {
+          if (typeof property === 'string') {
+            return property.toLowerCase().includes(keyword.toLowerCase())
+          }
+          return false
+        })
+      )
+    : restaurants
+  res.render('index', {
+    restaurants: matchedRestaurants,
+    cssPath: cssIndex,
+    keyword,
+  })
+})
+
+// 接收請求並處理: /restaurant/:id
+// 依照請求路徑上的id顯示對應的餐廳資訊
+// 將資料放入show.hbs並返回給客戶端
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  const restaurant = restaurants.find((restaurant) => restaurant.id.toString() === id)
+  res.render('show', { restaurant, cssPath: cssShow })
+})
+
+// 伺服器啟動並監聽port:3000
+app.listen(port, () => {
+  console.log(`express server is running on http://localhost:${port}`)
+})
